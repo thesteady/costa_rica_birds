5277a6196e25d2e8d1f9de8300a19f64a634d6d2
Laura Steadman <laura@quickleft.com>
2015-04-08 17:37:26 -0600

    Add a new/create route for making more birds.

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -4,6 +4,19 @@ class BirdsController < ApplicationController
     @birds = Bird.all
   end
 
+  def new
+    @bird = Bird.new
+  end
+
+  def create
+    bird = Bird.new(bird_params)
+    if bird.save
+      redirect_to bird_path(bird)
+    else
+      render :new
+    end
+  end
+
   def show
     @bird = Bird.find(params[:id])
   end
@@ -24,6 +37,6 @@ class BirdsController < ApplicationController
 
   private
   def bird_params
-    params.require(:bird).permit(:photo, :name, :description)
+    params.require(:bird).permit(:photo, :name, :description, :priority, :number_of_locations)
   end
 end
