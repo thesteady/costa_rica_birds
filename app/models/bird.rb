9c7df92778f6f5f16507d3017a30a5bb2ebba0e1
Laura Steadman <laura@quickleft.com>
2015-04-13 20:32:19 -0600

    Tweak views a bit more.

--- app/models/bird.rb
+++ app/models/bird.rb
@@ -1,6 +1,6 @@
 class Bird < ActiveRecord::Base
   # Paperclip:
-  has_attached_file :photo, styles: { medium: "300x300>"}, default_url: "/images/:style/missing.png"
+  has_attached_file :photo, styles: { medium: "300x300>", thumbnail: "100x100>"}, default_url: "/images/:style/missing.png"
   validates_attachment_content_type :photo, content_type: /\Aimage\/.*\Z/
 
   validates :name, presence: true
