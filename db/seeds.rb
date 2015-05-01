2980a4424a9adb51f8366540128650e2d55fc1b2
Laura Steadman <laura@quickleft.com>
2015-05-01 16:14:26 -0600

    update seeds.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,27 +1,19 @@
+require 'csv'
+
 locations = YAML.load_file(Rails.root.join('db', 'seeds', 'locations.yml')).map do |loc|
   Location.create name: loc['name'], ebird_id: loc['ebird_id'], website: loc['website']
 end
 
-# rank = 0
-# birds = YAML.load_file(Rails.root.join('db', 'seeds', 'birds.yml')).map do |name|
-#   rank += 1
-#   Bird.create(name: name, rank: rank, description: 'test description')
-# end
-
-# # birds.each do |bird|
-# #   BirdLocation.create bird: bird, location: locations.sample
-# # end
-
-require 'csv'
-
-csv_text = File.read(Rails.root.join('db', 'seeds', 'test_upload.csv'))
+csv_text = File.read(Rails.root.join('db', 'seeds', 'starter_subset.csv'))
 csv = CSV.parse(csv_text, headers: true)
 csv.each do |row|
   priority = row['priority'] == "Y" ? true : false
   photo_url = row['url']
   attribution = row['credit']
   name = row['name']
+
   puts "processing #{name}"
+
   Bird.create(
     name: name,
     priority: priority,
