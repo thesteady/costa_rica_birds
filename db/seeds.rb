b579b3bc67fc21793217c1a238cdb3858ca430d0
Laura Steadman <laura@quickleft.com>
2015-05-05 19:52:34 -0600

    Fix seeding so I get the attribution data (whoops).

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,19 +6,27 @@ end
 
 csv_text = File.read(Rails.root.join('db', 'seeds', 'starter_subset.csv'))
 csv = CSV.parse(csv_text, headers: true)
+
 csv.each do |row|
   priority = row['priority'] == "Y" ? true : false
   photo_url = row['photo_link']
-  attribution = row['credit']
+  attribution = row['photo_attribution']
   name = row['name']
 
   puts "processing #{name}"
 
-  Bird.create(
-    name: name,
-    priority: priority,
-    number_of_locations: row['number_of_locations'],
-    photo_remote_url: photo_url,
-    attribution: attribution
-  )
+  bird = Bird.find_by(name: name)
+
+  if bird
+    #update existing birds photo credits
+    bird.update({attribution: attribution})
+  else
+    Bird.create(
+      name: name,
+      priority: priority,
+      number_of_locations: row['number_of_locations'],
+      photo_remote_url: photo_url,
+      attribution: attribution
+    )
+  end
 end
